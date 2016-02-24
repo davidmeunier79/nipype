fb36c533cdf1be2692e0a2947a8b4174db89e47f
David Meunier (HP-EliteBook) <david.meunier@inserm.fr>
2016-02-24 15:33:32 +0100

    correction encoding

--- nipype/utils/misc.py
+++ nipype/utils/misc.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """Miscellaneous utility functions
@@ -228,7 +229,6 @@ def unflatten(in_list, prev_structure):
 
 
 ########################### ajout DM
-
 
 #### fonctions utilitaires vite faites ###################################
 
