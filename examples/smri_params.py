10f419de26abbcf10ce29974b3f6b4a8f46ab286
Annalisa Pascarella <a.pascarella@iac.cnr.it>
2016-03-29 14:16:54 -0400

    changed path

--- examples/smri_params.py
+++ examples/smri_params.py
@@ -7,7 +7,7 @@ Created on Mon Sep 21 22:44:00 2015
 import os
 import getpass
 
-test = True
+test = False
 is_nii = True # True if you have MRI files in nii format
 
 if getpass.getuser() == 'pasca':
@@ -16,7 +16,7 @@ if getpass.getuser() == 'pasca':
     MRI_path  = os.path.join(sbj_dir,"MRI")
     subjects_list = ['monk0002']  # ['dmn_mri','Monk1'] #['monk0002']  'dmn_mri'
 else:    
-    main_path = '/media/karim/DATAS/MEDITATION'         # data dir
+    main_path = '/home/karim/Documents/Fanny'           # data dir
     sbj_dir   =  os.path.join(main_path,"FSF")          # where FS creates sbj dir           
     MRI_path  =  main_path                              # MRI dir
     
@@ -27,8 +27,9 @@ else:
 #                         'haimo','laupa','levma','mahan','marle','merly','mesma','moryv','ricro',
 #                         'rimso','rougw','sanga','torgu','vanso','virje'] 
 #    
-        subjects_list = ['monk0003','monk0004','monk0004','monk0005','monk0006','monk0007','monk0008','monk0009','monk0010',
-                         'monk0011','monk0012']
+#        subjects_list = ['monk0003','monk0004','monk0004','monk0005','monk0006','monk0007','monk0008','monk0009','monk0010',
+#                         'monk0011','monk0012']
+        subjects_list = ['S02','S03', "S04",'S05','S06', "S07",'S08','S09', "S10",'S11','S12', "S13",'S14','S15', "S16"] 
         
 # dir names in main_path=sbj_dir
 FS_WF_name    = "segmentation_workflow" 
