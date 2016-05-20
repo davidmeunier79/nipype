5c6ef2f88ea10b219939fb4eddd77736f541e658
Dmitrii Altukhov <dmalt@me.com>
2016-05-20 17:41:22 -0400

    new protocol

--- examples/smri_params.py
+++ examples/smri_params.py
@@ -7,37 +7,45 @@ Created on Mon Sep 21 22:44:00 2015
 import os
 import getpass
 
-test = True
-is_nii = True # True if you have MRI files in nii format
+test = False
+is_nii = True  # True if you have MRI files in nii format
 
 if getpass.getuser() == 'pasca':
     main_path = '/home/pasca/Dropbox/work/karim/data/'
-    sbj_dir   = '/home/pasca/Science/research/MEG/work/subjects/'    
-    MRI_path  = os.path.join(sbj_dir,"MRI")
-    subjects_list = ['monk0002']  # ['dmn_mri','Monk1'] #['monk0002']  'dmn_mri'
-else:    
-    main_path = '/media/karim/DATAS/MEDITATION'         # data dir
-    sbj_dir   =  os.path.join(main_path,"FSF")          # where FS creates sbj dir           
-    MRI_path  =  main_path                              # MRI dir
-    
-    if test:
-        subjects_list = ['monk0002'] # ['MORYV','SANGA','VANSO','VIRJE'] 
-    else:
-#        subjects_list = ["balai",'benba','casla','doble','droco','duple','frapa',
-#                         'haimo','laupa','levma','mahan','marle','merly','mesma','moryv','ricro',
-#                         'rimso','rougw','sanga','torgu','vanso','virje'] 
-#    
-        subjects_list = ['monk0003','monk0004','monk0004','monk0005','monk0006','monk0007','monk0008','monk0009','monk0010',
-                         'monk0011','monk0012']
-        
-# dir names in main_path=sbj_dir
-FS_WF_name    = "segmentation_workflow" 
-BEM_WF_name   = "watershed_bem" 
-MAIN_WF_name  = "main_workflow" 
-    
-    
-if test == True:
-    FS_WF_name   = FS_WF_name   + '_test'
-    BEM_WF_name  = BEM_WF_name  + '_test'
-    MAIN_WF_name = MAIN_WF_name + '_test'
+    sbj_dir = '/home/pasca/Science/research/MEG/work/subjects/'
+    MRI_path = os.path.join(sbj_dir, "MRI")
+    subjects_list = ['monk0002']  # ['dmn_mri','Monk1'] ['monk0002']  'dmn_mri'
 
+else:
+#    main_path = '/home/karim/Documents/Fanny'           # data dir
+    main_path = '/home/karim/Documents/Blindsight'  # data dir
+    sbj_dir = os.path.join(main_path, "FSF")        # where FS creates sbj dir
+    MRI_path = main_path                            # MRI dir
+
+    if test:
+        subjects_list = ['monk0002']  # ['MORYV','SANGA','VANSO','VIRJE']
+
+    else:
+        '''
+        subjects_list = ["balai",'benba','casla','doble','droco','duple',
+                         'frapa','haimo','laupa','levma','mahan','marle',
+                         'merly','mesma','moryv','ricro','rimso','rougw',
+                         'sanga','torgu','vanso','virje']
+
+        subjects_list = ['monk0003','monk0004','monk0004','monk0005',
+                         'monk0006','monk0007','monk0008','monk0009',
+                         'monk0010','monk0011','monk0012']
+        subjects_list = ['S02','S03', "S04",'S05','S06', "S07",'S08','S09',
+                         "S10",'S11','S12', "S13",'S14','S15', "S16"]
+        '''
+        subjects_list = ['sbj_1']
+
+# dir names in main_path=sbj_dir
+FS_WF_name = "segmentation_workflow"
+BEM_WF_name = "watershed_bem"
+MAIN_WF_name = "main_workflow"
+
+if test:
+    FS_WF_name = FS_WF_name + '_test'
+    BEM_WF_name = BEM_WF_name + '_test'
+    MAIN_WF_name = MAIN_WF_name + '_test'
