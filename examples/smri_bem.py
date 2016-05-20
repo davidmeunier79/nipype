6de1e5ed597e3ec53698155015d562de66933b9b
Dmitrii Altukhov <dmalt@me.com>
2016-05-20 17:40:54 -0400

    new rename of the surf files

--- examples/smri_bem.py
+++ examples/smri_bem.py
@@ -49,7 +49,8 @@ def copy_surfaces(sbj_id, mesh_files):
 
     for i in range(len(surf_names)):
         os.system('cp %s %s' %(op.join(surface_dir,sbj_id + '_' + surf_names[i]), 
-                               op.join(bem_dir,sbj_id + '-' + new_surf_names[i])))
+#                               op.join(bem_dir,sbj_id + '-' + new_surf_names[i])))
+                               op.join(bem_dir, new_surf_names[i])))
    
     return sbj_id    
     
@@ -62,4 +63,6 @@ bem_workflow.connect(infosource, 'subject_id',  bem, 'subject_id')
 bem_workflow.connect(infosource, 'subject_id',  copy_bem_surf, 'sbj_id')
 bem_workflow.connect(bem,        'mesh_files',  copy_bem_surf, 'mesh_files')
 
-bem_workflow.run() 
+bem_workflow.write_graph(graph2use='colored')
+bem_workflow.config['execution'] = {'remove_unnecessary_outputs':'false'}
+bem_workflow.run(plugin='MultiProc', plugin_args={'n_procs' : 8}) 
